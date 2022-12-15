3d623513496a75ee3a6fc4f73ca6e75a4e709504
doom <phramov945@gmail.com>
2022-12-15 04:19:57 +0300

    Build and Deploy

--- logs.py
+++ logs.py
@@ -12,11 +12,11 @@ logger_api = logging.getLogger("api")
 logger_main = logging.getLogger("main")
 
 # Creating log handlers and setting formatters for them
-file_handler_api = logging.FileHandler(API_LOG_PATH, mode='w')
-file_handler_api.setFormatter(formatter)
-file_handler_main = logging.FileHandler(MAIN_LOG_PATH, mode='w')
-file_handler_main.setFormatter(formatter)
+# file_handler_api = logging.FileHandler(API_LOG_PATH, mode='w')
+# file_handler_api.setFormatter(formatter)
+# file_handler_main = logging.FileHandler(MAIN_LOG_PATH, mode='w')
+# file_handler_main.setFormatter(formatter)
 
 # Adding created handlers for loggers
-logger_api.addHandler(file_handler_api)
-logger_main.addHandler(file_handler_main)
+# logger_api.addHandler(file_handler_api)
+# logger_main.addHandler(file_handler_main)
